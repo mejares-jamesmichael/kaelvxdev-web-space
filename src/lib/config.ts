bcd58d7d781d5b48a1db4190e642253c2680da81
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2026-02-11 04:11:23 +0800

    refactor: centralize portfolio configuration and constants

---
+++ src/lib/config.ts
@@ -0,0 +1,17 @@
+export const config = {
+  name: 'James Michael',
+  title: 'Backend & DevOps Engineer',
+  email: 'jamesmichaelmejares@gmail.com',
+  socials: {
+    github: 'https://github.com/mejares-jamesmichael',
+    linktree: 'https://linktr.ee/kaelvxd'
+  },
+  skills: [
+    'Linux',
+    'AWS',
+    'CI/CD',
+    'Docker',
+    'GitHub Actions'
+  ],
+  cvPath: '/James-Michael-Mejares.pdf'
+};
