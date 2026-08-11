c95365117e4380f5adb11f74ebd74f6e1203ad99
James Michael Mejares <jmmejares.xoointern@gmail.com>
2026-08-11 17:02:13 +0800

    refactor(config): add verify link and remove certification id
    this is for security purposes

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -21,8 +21,9 @@ export const config = {
       issuer: 'TESDA',
       issued: 'May 2023',
       expires: 'May 2028',
-      credentialId: '23131602004517',
-      verifyUrl: null,
+      verifyUrl:
+        'https://t2mis.tesda.gov.ph/Learners/S/3700460046006A006B005A0042007600590062006C0047004700300032006900530066007A0066002F0056006E004C0030005000610079002F004C00420054006B00670039007100440075006D0077004B00760038003D00',
+      certificateUrl: 'https://drive.google.com/file/d/155U3JbJb_RHIlgCkQqA8Xi-qkZjPW6Lo/view?usp=sharing',
       credlyBadgeId: null
     },
     {
@@ -31,8 +32,8 @@ export const config = {
       issuer: 'Cisco',
       issued: 'Aug 2026',
       expires: null,
-      credentialId: 'c3a6119a-a6bc-4345-ab39-4725dd24dc01',
       verifyUrl: 'https://www.credly.com/badges/c3a6119a-a6bc-4345-ab39-4725dd24dc01',
+      certificateUrl: null,
       credlyBadgeId: 'c3a6119a-a6bc-4345-ab39-4725dd24dc01'
     }
   ],
