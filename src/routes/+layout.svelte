b0b2bb9175056976096135bfba8f332b59be8cd7
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2026-08-12 22:17:07 +0800

    refactor(layout): clean the file and remove the references of status line

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script>
   import '../app.css';
-  import TelemetryBar from '$lib/components/TelemetryBar.svelte';
+  // ponytail: Removed TelemetryBar because it's redundant (YAGNI)
 </script>
 
-<div class="min-h-screen bg-black text-white selection:bg-white selection:text-black relative pb-8">
+<div class="min-h-screen bg-black text-white selection:bg-white selection:text-black relative">
 
   <!-- Gradient Background -->
   <div class="fixed inset-0 bg-[radial-gradient(circle_at_80%_0%,_#1c1c1c_0%,_#000000_70%)] pointer-events-none z-0"></div>
@@ -11,6 +11,4 @@
   <div class="relative z-10">
     <slot />
   </div>
-  
-  <TelemetryBar />
 </div>
